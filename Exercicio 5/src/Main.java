593288cf31db51fccace954bf41f8337713c8a66
Domingueszz <159201898+Domingueszz@users.noreply.github.com>
2024-02-15 20:04:48 -0300

    Update Main.java

--- Exercicio 5/src/Main.java
+++ Exercicio 5/src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
         Scanner in = new Scanner(System.in);
         System.out.println("Digite uma quilolmetragem:");
         number1 = in.nextDouble();
-        result = (number1 * 1000) / 3.6;
+        result = number1 * 1000 / 3.6;
         System.out.println("O Km/h em m/s é: " + result);
     }
 }
